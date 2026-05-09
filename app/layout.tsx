79e97b3afe0710e4db8af9f062bd5d05cc756f20
Nego <abetnego.kristiawan@gmail.com>
2026-05-09 13:25:18 +0800

    Update metadata to use correct favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,13 @@ const sora = Sora({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Creativorium Invoice',
   description: 'Invoice generator for Creativorium',
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon.ico', sizes: '48x48', type: 'image/x-icon' },
+    ],
+  },
 }
 
 export default function RootLayout({
